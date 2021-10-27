57ae9c8a14e9ac0c0d610ec7612a1e85ba05432c
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-27 16:38:45 +0200

    fix bugs and add function addUserNumbers

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -17,32 +17,69 @@
 let alertNumbers;
 let counter = 5;
 let seconds = 30;
+let timer;
+let insertNumbers;
+const randomListNumbers = [];
+const userAddNumbers = [];
 
-const randomNumber = setInterval(generateRandomNumbers, 1000);
-const timer = setInterval(passSeconds, 1000);
+const randomelement = document.getElementById("random");
+const startElement = document.getElementById("start");
+
+const showRandomNumber = setInterval(showNumbers, 2000);
+const hideRandomNumber = setInterval(hideNumbers, 4000);
 
 
 
 
 function generateRandomNumbers() {
-    alertNumbers = Math.floor(Math.random() * 100);
-    alert(alertNumbers);
-    counter--
-    console.log(alertNumbers);
-    //console.log(counter);
-    if(counter == 0) {
-       clearInterval(randomNumber)
-    }
-
+    for (let i = 0; i < 5; i++) {
+   alertNumbers = Math.floor(Math.random() * 100);
+   //console.log(alertNumbers)
+   if(!randomListNumbers.includes(alertNumbers)) {
+        randomListNumbers.push(alertNumbers)
+        //console.log(randomListNumbers);
+    } 
+    
+  }
+  return randomListNumbers
 }
 
-function passSeconds() {
-    seconds--
+const randomNumbers = generateRandomNumbers()
+console.log(randomNumbers);
+//alert(randomNumbers);
+
+function showNumbers() {
+    randomelement.innerHTML = randomNumbers
+}
+
+function hideNumbers() {
+    randomelement.style.display = "none"
+}
+
+
+startElement.addEventListener("click", function() {
+    timer = setInterval(function (){
+     seconds--
     // console.log(seconds);
     document.getElementById("seconds").innerHTML = seconds
 
     if(seconds == 0) {
        clearInterval(timer)
         document.getElementById("seconds").innerHTML = ""
+        addUserNumbers()
     }
+
+}, 1000)
+ });  
+ 
+ 
+ function addUserNumbers() {
+    for (let i = 0; i < 5; i++) {
+   insertNumbers = Number(prompt("inserisci ogni numero che hai visto"));
+   if(!userAddNumbers.includes(insertNumbers)) {
+        userAddNumbers.push(insertNumbers)
+        console.log(userAddNumbers);
+    }
+    
+}
 }
